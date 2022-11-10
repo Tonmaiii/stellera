ee963ed77c52df7866191fd7248fa3215738b5bf
Tonmaiii <pruek.rodsawat@gmail.com>
2022-11-10 15:21:40 +0700

    device orientation

--- src/game/engine.ts
+++ src/game/engine.ts
@@ -278,8 +278,8 @@ document.addEventListener('touchend', e => {
 
 window.addEventListener('deviceorientation', e => {
     ra = (e.alpha * Math.PI) / 180
-    dec = ((e.gamma - 90) * Math.PI) / 180
-    rotate = (-e.beta * Math.PI) / 180
+    dec = ((Math.abs(e.gamma) - 90) * Math.PI) / 180
+    rotate = (e.beta * Math.PI) / 180
 })
 
 const setStarsVertexPointer = (gl, program, vertexSize) => {
